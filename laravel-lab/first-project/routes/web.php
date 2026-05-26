8943c740cd4597e030f37475c8e4f378ce3d93c6
Giancarlo Cicellyn Comneno <126195429+gcomneno@users.noreply.github.com>
2026-05-26 16:57:19 +0200

    Add Laravel lesson 05 view data example

--- laravel-lab/first-project/routes/web.php
+++ laravel-lab/first-project/routes/web.php
@@ -1,19 +1,12 @@
 <?php
-/*  Qui si introduce la facade. In Laravel una facade sembra una classe usata staticamente, tipo: Route::get(...)
-    ma dietro le quinte Laravel sta delegando quella chiamata a un oggetto reale gestito dal container.
-    Quindi non è semplicemente “chiamo un metodo statico”; è una scorciatoia comoda per accedere a servizi già configurati.
-*/
+
 use Illuminate\Support\Facades\Route;
 
-// Il secondo argomento è una closure, cioè una "funzione anonima" eseguita quando quella rotta viene visitata.
-/* In quesot esempio la root home-page viene indirizzata verso il rendering della view denominata "welcome"
 Route::get('/', function () {
-    return view('welcome');
-});
-*/
-
-Route::get('/', function () {
-    return view('pages.home');
+    return view('pages.home', [
+        'greeting' => 'Hello',
+        'showGreeting' => true,
+    ]);
 })->name('home');
 
 Route::get('/about', function () {
