5aaf162a396f02297aabdf6273ad173e29702ca2
Giancarlo Cicellyn Comneno <126195429+gcomneno@users.noreply.github.com>
2026-07-22 08:27:28 +0200

    Add Laravel lesson 17 validation error display

--- laravel-lab/first-project/resources/views/projects/create.blade.php
+++ laravel-lab/first-project/resources/views/projects/create.blade.php
@@ -1,23 +1,25 @@
-<div>
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>New project</title>
-    </head>
-    <body>
-        <h1>New project</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>New project</title>
+</head>
+<body>
+    <h1>New project</h1>
 
-        <form action="/projects" method="POST">
-            @csrf
+    <form action="/projects" method="POST">
+        @csrf
 
-            <div>
-                <label for="name">Name</label>
-                <input id="name" type="text" name="name">
-            </div>
+        <div>
+            <label for="name">Name</label>
+            <input id="name" type="text" name="name" value="{{ old('name') }}">
 
-            <button type="submit">Create project</button>
-        </form>
-    </body>
-    </html>
-</div>
+            @error('name')
+                <p>{{ $message }}</p>
+            @enderror
+        </div>
+
+        <button type="submit">Create project</button>
+    </form>
+</body>
+</html>
