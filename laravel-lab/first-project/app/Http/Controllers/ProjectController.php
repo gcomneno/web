82185e07f9f5a20aa34ac73e33227af869f1b6ea
Giancarlo Cicellyn Comneno <126195429+gcomneno@users.noreply.github.com>
2026-08-06 19:16:06 +0200

    Add Laravel lesson 20 Eloquent ordering

--- laravel-lab/first-project/app/Http/Controllers/ProjectController.php
+++ laravel-lab/first-project/app/Http/Controllers/ProjectController.php
@@ -10,7 +10,7 @@ class ProjectController extends Controller
     public function index()
     {
         return view('projects.index', [
-            'projects' => Project::get(),
+            'projects' => Project::latest()->get(),
         ]);
     }
 
