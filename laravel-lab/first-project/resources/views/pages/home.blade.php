3b36a7c3dc2ae7650ef97671a17e2749e918d373
Giancarlo Cicellyn Comneno <126195429+gcomneno@users.noreply.github.com>
2026-08-21 14:18:23 +0200

    Add Laravel lesson 23 Vite assets

--- laravel-lab/first-project/resources/views/pages/home.blade.php
+++ laravel-lab/first-project/resources/views/pages/home.blade.php
@@ -3,6 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <title>{{ config('app.name') }}</title>
+
+    @vite([
+        'resources/css/app.css',
+        'resources/js/app.js',
+    ])
 </head>
 <body>
     @if ($showGreeting)
